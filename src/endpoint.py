6484c3d6b2e6512fc581b2943783c6fee6f38743
Dhruv Batheja <dhruv.life@hotmail.com>
2018-04-14 19:31:05 +0200

    Removed unused imports

--- src/endpoint.py
+++ src/endpoint.py
@@ -1,13 +1,8 @@
 from predict import talk
 
-
-
-
-from PIL import Image
 from flask import Flask, request
 import json
 from waitress import serve
-import base64
 app = Flask(__name__)
 
 from flask import render_template
